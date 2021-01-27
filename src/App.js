3af3daa70abf17106d96386e70088e2aed962d1a
Ismail-Lh <ismailsaad03@gmail.com>
2021-01-27 01:07:18 +0100

    add the initiall files

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App () {
+  return <h4>comfy sloth starter</h4>;
+}
+
+export default App;
