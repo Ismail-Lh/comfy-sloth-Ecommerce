84ef47ca8cc1b38022f315d327b53c64f805b4d4
Ismail-Lh <ismailsaad03@gmail.com>
2021-01-28 19:57:19 +0100

    Create the ErrorPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function ErrorPage () {
-   return (
-      <h1>
-         Error Page!!!!
-      </h1>
-   );
+import styled from "styled-components/macro";
+
+function ErrorPage() {
+	return (
+		<Wrapper className="page-100">
+			<section>
+				<h1>404</h1>
+				<h3>Sorry, the page you tried cannot be found</h3>
+				<Link to="/" className="btn">
+					back home
+				</Link>
+			</section>
+		</Wrapper>
+	);
 }
+
+const Wrapper = styled.main`
+	background: var(--clr-primary-10);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	h1 {
+		font-size: 10rem;
+	}
+	h3 {
+		text-transform: none;
+		margin-bottom: 2rem;
+	}
+`;
 
 export default ErrorPage;
