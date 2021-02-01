ea2edc2b1c24416cbdcb7f75bae0617ee0b6d35e
Ismail-Lh <ismailsaad03@gmail.com>
2021-02-01 18:06:05 +0100

    Recreate the Stars compo with programing method

--- src/components/Stars/Stars.jsx
+++ src/components/Stars/Stars.jsx
@@ -4,55 +4,25 @@ import styled from "styled-components/macro";
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 
 const Stars = ({ stars, reviews }) => {
+	const tempStars = Array.from({ length: 5 }, (_, idx) => {
+		const num = idx + 0.5;
+
+		return (
+			<span>
+				{stars >= idx + 1 ? (
+					<BsStarFill />
+				) : stars >= num ? (
+					<BsStarHalf />
+				) : (
+					<BsStar />
+				)}
+			</span>
+		);
+	});
+
 	return (
 		<Wrapper>
-			<div className="stars">
-				<span>
-					{stars >= 1 ? (
-						<BsStarFill />
-					) : stars >= 0.5 ? (
-						<BsStarHalf />
-					) : (
-						<BsStar />
-					)}
-				</span>
-				<span>
-					{stars >= 2 ? (
-						<BsStarFill />
-					) : stars >= 1.5 ? (
-						<BsStarHalf />
-					) : (
-						<BsStar />
-					)}
-				</span>
-				<span>
-					{stars >= 3 ? (
-						<BsStarFill />
-					) : stars >= 2.5 ? (
-						<BsStarHalf />
-					) : (
-						<BsStar />
-					)}
-				</span>
-				<span>
-					{stars >= 4 ? (
-						<BsStarFill />
-					) : stars >= 3.5 ? (
-						<BsStarHalf />
-					) : (
-						<BsStar />
-					)}
-				</span>
-				<span>
-					{stars >= 5 ? (
-						<BsStarFill />
-					) : stars >= 4.5 ? (
-						<BsStarHalf />
-					) : (
-						<BsStar />
-					)}
-				</span>
-			</div>
+			<div className="stars">{tempStars}</div>
 			<p className="reviews">({reviews} customer reviews)</p>
 		</Wrapper>
 	);
