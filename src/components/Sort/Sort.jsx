0eeb6871cdfdf932923d8275c0a2f616239119ec
Ismail-Lh <ismailsaad03@gmail.com>
2021-02-02 19:15:12 +0100

    Create the Sort Compo (basic setup) for the ProductsPage

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -5,7 +5,33 @@ import { BsFillGridFill, BsList } from "react-icons/bs";
 import { Wrapper } from "./SortStyles";
 
 const Sort = () => {
-	return <h4>sort </h4>;
+	const { filtered_products: products, grid_view } = useFilterContext();
+
+	return (
+		<Wrapper>
+			<div className="btn-container">
+				<button type="button" className={`${grid_view && "active"}`}>
+					<BsFillGridFill />
+				</button>
+				<button type="button" className={`${grid_view || "active"}`}>
+					<BsList />
+				</button>
+			</div>
+
+			<p>{products.length} products found</p>
+			<hr />
+
+			<form>
+				<label htmlFor="sort">Sort by</label>
+				<select name="sort" id="sort" className="sort-input">
+					<option value="price-lowest">price (lowest)</option>
+					<option value="price-highest">price (highest)</option>
+					<option value="name-a">name (a - z)</option>
+					<option value="name-z">name (z - z)</option>
+				</select>
+			</form>
+		</Wrapper>
+	);
 };
 
 export default Sort;
--- src/components/Sort/SortStyles.js
+++ src/components/Sort/SortStyles.js
@@ -1,64 +1,67 @@
-import styled from 'styled-components/macro'
+import styled from "styled-components/macro";
 
 export const Wrapper = styled.section`
-  display: grid;
-  grid-template-columns: auto auto 1fr auto;
-  align-items: center;
-  margin-bottom: 2rem;
-  column-gap: 2rem;
-  @media (max-width: 576px) {
-    display: grid;
-    grid-template-columns: 1fr;
-    row-gap: 0.75rem;
-    .btn-container {
-      width: 50px;
-    }
-    label {
-      display: inline-block;
-      margin-right: 0.5rem;
-    }
-  }
-  @media (min-width: 768px) {
-    column-gap: 2rem;
-  }
-  p {
-    text-transform: capitalize;
-    margin-bottom: 0;
-  }
+	display: grid;
+	grid-template-columns: auto auto 1fr auto;
+	align-items: center;
+	margin-bottom: 2rem;
+	column-gap: 2rem;
+	@media (max-width: 576px) {
+		display: grid;
+		grid-template-columns: 1fr;
+		row-gap: 0.75rem;
+		.btn-container {
+			width: 50px;
+		}
+		label {
+			display: inline-block;
+			margin-right: 0.5rem;
+		}
+	}
+	@media (min-width: 768px) {
+		column-gap: 2rem;
+	}
+	p {
+		text-transform: capitalize;
+		margin-bottom: 0;
+	}
 
-  .btn-container {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    column-gap: 0.5rem;
-    button {
-      background: transparent;
-      border: 1px solid var(--clr-black);
-      color: var(--clr-black);
-      width: 1.5rem;
-      border-radius: var(--radius);
-      height: 1.5rem;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      cursor: pointer;
-      svg {
-        font-size: 1rem;
-      }
-    }
-    .active {
-      background: var(--clr-black);
-      color: var(--clr-white);
-    }
-  }
+	.btn-container {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		column-gap: 0.5rem;
+		button {
+			background: transparent;
+			border: 1px solid var(--clr-black);
+			color: var(--clr-black);
+			width: 1.5rem;
+			border-radius: var(--radius);
+			height: 1.5rem;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			cursor: pointer;
+			svg {
+				font-size: 1rem;
+			}
+		}
+		.active {
+			background: var(--clr-black);
+			color: var(--clr-white);
+		}
+	}
 
-  .sort-input {
-    border-color: transparent;
-    font-size: 1rem;
-    text-transform: capitalize;
-    padding: 0.25rem 0.5rem;
-  }
-  label {
-    font-size: 1rem;
-    text-transform: capitalize;
-  }
-`
+	.sort-input {
+		border-color: transparent;
+		font-size: 1rem;
+		text-transform: capitalize;
+		padding: 0.25rem 0.5rem;
+		outline: 0;
+	}
+	label {
+		font-size: 1rem;
+		text-transform: capitalize;
+		display: inline-block;
+		margin-right: 0.5rem;
+	}
+`;
