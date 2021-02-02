48d006d4cb629e8e84fb170522cd7e455388da82
Ismail-Lh <ismailsaad03@gmail.com>
2021-02-02 17:11:53 +0100

    FilterContext setup

---
+++ src/contexts/filter_context.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useContext, useReducer, createContext } from "react";
+
+import FilterReducer from "../reducers/filter_reducer";
+import { useProductsContext } from "./products_context";
+
+import {
+	LOAD_PRODUCTS,
+	SET_GRIDVIEW,
+	SET_LISTVIEW,
+	UPDATE_SORT,
+	SORT_PRODUCTS,
+	UPDATE_FILTERS,
+	FILTER_PRODUCTS,
+	CLEAR_FILTERS
+} from "../actions";
+
+const initialState = {
+	filtered_products: [],
+	all_products: []
+};
+
+const FilterContext = createContext();
+
+export const FilterProvider = ({ children }) => {
+	const { all_products } = useProductsContext();
+	const [state, dispatch] = useReducer(FilterReducer, initialState);
+
+	useEffect(() => {
+		dispatch({ type: LOAD_PRODUCTS, payload: all_products });
+	}, [all_products]);
+
+	return (
+		<FilterContext.Provider value="filter context">
+			{children}
+		</FilterContext.Provider>
+	);
+};
+// make sure use
+export const useFilterContext = () => {
+	return useContext(FilterContext);
+};
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 import { ProductsProvider } from "./contexts/products_context";
+import { FilterProvider } from "./contexts/filter_context";
 import App from "./App";
 
 ReactDOM.render(
 	<ProductsProvider>
-		<App />
+		<FilterProvider>
+			<App />
+		</FilterProvider>
 	</ProductsProvider>,
 	document.getElementById("root")
 );
