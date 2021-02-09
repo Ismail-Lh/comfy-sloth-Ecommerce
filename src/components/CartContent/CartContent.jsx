8a40e60ab67c1c316874da34dfcaa6cc9a53e08d
Ismail <ismailsaad03@gmail.com>
2021-02-09 17:55:48 +0100

    Create the CartContent compo setup

--- src/components/CartContent/CartContent.jsx
+++ src/components/CartContent/CartContent.jsx
@@ -7,7 +7,29 @@ import { useCartContext } from '../../contexts/cart_context';
 import { CartColumns, CartItem, CartTotals } from '../';
 
 const CartContent = () => {
-  return <h4>CartContent </h4>;
+  const { cart, clearCart } = useCartContext();
+  return (
+    <Wrapper className='section section-center'>
+      <CartColumns />
+      {cart.map((item) => (
+        <CartItem key={item.id} {...item} />
+      ))}
+
+      <hr />
+
+      <div className='link-container'>
+        <Link to='/products' className='link-btn'>
+          continue shopping
+        </Link>
+
+        <button className='link-btn clear-btn' onClick={clearCart}>
+          clear shopping cart
+        </button>
+      </div>
+
+      <CartTotals />
+    </Wrapper>
+  );
 };
 
 export default CartContent;
