e65c0eeb6e00e9643a3e95bce8b5b56691739682
Ismail <ismailsaad03@gmail.com>
2021-02-10 17:55:45 +0100

    setup auth0 provider

--- src/index.js
+++ src/index.js
@@ -6,15 +6,26 @@ import { ProductsProvider } from './contexts/products_context';
 import { FilterProvider } from './contexts/filter_context';
 import { CartProvider } from './contexts/cart_context';
 
+import { Auth0Provider } from '@auth0/auth0-react';
+
 import App from './App';
 
+// dev-umf9vfah.eu.auth0.com
+// lKbHSCxJckyiLij9oTayNbER5WHeajxs
+
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain='dev-umf9vfah.eu.auth0.com'
+    clientId='lKbHSCxJckyiLij9oTayNbER5WHeajxs'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'>
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
   document.getElementById('root')
 );
